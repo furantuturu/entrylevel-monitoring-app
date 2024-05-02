d0de62d245bc6bdfab7f973259af9ac8fab8c571
testing <fuwansuu@gmail.com>
2024-05-03 00:26:25 +0800

    updates v1.4

--- app/src/main/java/com/example/finalact/MainActivity.java
+++ app/src/main/java/com/example/finalact/MainActivity.java
@@ -9,11 +9,35 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button teacherLogin;
+    Button studentLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teacherLogin = findViewById(R.id.btnTeacherLogin);
+        studentLogin = findViewById(R.id.btnStudentLogin);
+
+        buttonListener();
+    }
+
+    private void buttonListener() {
+        teacherLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        studentLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StudentLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
