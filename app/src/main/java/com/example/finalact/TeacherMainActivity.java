ca1e699c7aee9ee989afadc9105a75e5e8866f19
testing <fuwansuu@gmail.com>
2024-05-02 23:04:08 +0800

    updates v1.3

--- app/src/main/java/com/example/finalact/TeacherMainActivity.java
+++ app/src/main/java/com/example/finalact/TeacherMainActivity.java
@@ -7,17 +7,25 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import java.net.URI;
+import java.util.List;
 
 public class TeacherMainActivity extends AppCompatActivity {
-    TextView message;
-    private Uri uri;
+    TextView studentname;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_main);
 
-        message = findViewById(R.id.)
+        studentname = findViewById(R.id.TVstudentname);
+        uri = getIntent().getData();
+
+        if (uri != null) {
+            List<String> parameters = uri.getPathSegments();
+            String param = parameters.get(parameters.size() - 1);
+            studentname.setText(param);
+        }
 
     }
 }
