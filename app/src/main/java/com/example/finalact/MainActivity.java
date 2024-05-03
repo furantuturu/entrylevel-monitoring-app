35631f58e63e53c3d4f2d24f1fbdd8bd6e17db33
SchizoRamen <67666864+takotatsuoji@users.noreply.github.com>
2024-05-03 12:58:00 +0800

    updatesss

--- app/src/main/java/com/example/finalact/MainActivity.java
+++ app/src/main/java/com/example/finalact/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.finalact;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn2;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn2 = findViewById(R.id.button2);
+
+        addButtonListener();
+    }
+
+    private void addButtonListener() {
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                intent = new Intent(MainActivity.this, StudentLoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
