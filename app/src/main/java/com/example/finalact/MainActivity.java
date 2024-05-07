06e24298484f6df6a05be3f88a37a4a9183bff8a
testing <fuwansuu@gmail.com>
2024-05-07 16:06:45 +0800

    intent redirection to subject selection activity

--- app/src/main/java/com/example/finalact/MainActivity.java
+++ app/src/main/java/com/example/finalact/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button teacherLogin;
@@ -27,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
         teacherLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, SubjectSelectionActivity.class);
+                startActivity(intent);
             }
         });
 
