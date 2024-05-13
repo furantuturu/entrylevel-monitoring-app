523da7f1a732519213e73684f7c2bc4bb6d96339
testing <fuwansuu@gmail.com>
2024-05-13 16:02:40 +0800

    now added login functionality for handling logins

--- app/src/main/java/com/example/finalact/StudentLoginActivity.java
+++ app/src/main/java/com/example/finalact/StudentLoginActivity.java
@@ -5,30 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class StudentLoginActivity extends AppCompatActivity {
-
-    TextView txtView;
+    EditText etStuName, etStuPwd;
+    Button btnStuSignup, btnStuLogin;
+    TextView fgtPwdTec;
     Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
 
-        txtView = findViewById(R.id.forgot_pass);
+        etStuName = findViewById(R.id.ETstuname);
+        etStuPwd = findViewById(R.id.ETstupwd);
+        btnStuLogin = findViewById(R.id.BTNstulogin);
+        btnStuSignup = findViewById(R.id.BTNstusignup);
+        fgtPwdTec = findViewById(R.id.forgot_pass_stu);
 
-
-        addTxtListener();
+        listeners();
     }
 
-    private void addTxtListener() {
-        txtView.setOnClickListener(new View.OnClickListener() {
+    private void listeners() {
+        fgtPwdTec.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 intent = new Intent(StudentLoginActivity.this , ForgotPasswordStudent.class);
                 startActivity(intent);
+            }
+        });
 
+        btnStuLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etStuName.getText().toString().equals("") || !etStuPwd.getText().toString().equals("")) {
+
+                } else {
+                    Toast.makeText(StudentLoginActivity.this, "Don't leave the fields blank", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnStuSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(StudentLoginActivity.this, SignupActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/finalact/TeacherLoginActivity.java
+++ app/src/main/java/com/example/finalact/TeacherLoginActivity.java
@@ -4,25 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class TeacherLoginActivity extends AppCompatActivity {
-
-    TextView txtView;
+    EditText etTecName, etTecPwd;
+    Button btnTecLogin, btnTecSignup;
+    TextView fgtPwdTec;
     ImageView imageView;
+    Intent intent;
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_login);
 
-        txtView = findViewById(R.id.forgotPassword);
+        etTecName = findViewById(R.id.ETtecname);
+        etTecPwd = findViewById(R.id.ETtecpwd);
+        btnTecLogin = findViewById(R.id.BTNteclogin);
+        btnTecSignup = findViewById(R.id.BTNtecsignup);
+        fgtPwdTec = findViewById(R.id.forgotPasswordTec);
         imageView = findViewById(R.id.teacherImg);
 
         Animation fadein = AnimationUtils.loadAnimation(this, R.anim.fadein);
@@ -30,18 +37,30 @@ public class TeacherLoginActivity extends AppCompatActivity {
 
 
 
-        addTxtView();
+        listeners();
     }
-    public void addTxtView(){
-        txtView.setOnClickListener(new View.OnClickListener() {
+    public void listeners(){
+        fgtPwdTec.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent();
-
                 intent = new Intent(TeacherLoginActivity.this, ForgotPasswordTeacher.class);
                 startActivity(intent);
 
+            }
+        });
+
+        btnTecLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnTecSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(TeacherLoginActivity.this, SignupActivity.class);
+                startActivity(intent);
             }
         });
     }
