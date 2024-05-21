fa03dae4ced75a68a7f379225365e9ecc84ba687
testing <fuwansuu@gmail.com>
2024-05-21 15:37:36 +0800

    added sharedpreference to record the student name to delete data when the app is removed from recent task

--- app/src/main/java/com/example/finalact/MyService.java
+++ app/src/main/java/com/example/finalact/MyService.java
@@ -1,13 +1,17 @@
 package com.example.finalact;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
-public class MyService extends Service  {
+public class MyService extends Service {
+    private String stuname;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -17,6 +21,15 @@ public class MyService extends Service  {
     @Override
     public void onTaskRemoved(Intent rootIntent) {
         super.onTaskRemoved(rootIntent);
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPrefs", WelcomeActivity.MODE_PRIVATE);
+        this.stuname = sharedPreferences.getString("stuname", null);
+
+        WelcomeActivity ins = new WelcomeActivity();
+        ins.deleteData(this.stuname);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
         this.stopSelf();
     }
 }
